5b53ec0056b093859438cc75278fc17b4510af5b
Masato Nitta <newmtube07@gmail.com>
2025-10-07 23:55:34 +0900

    update

--- react-app/src/components/Header.js
+++ react-app/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
 
 const Header = ({ user, onLogout }) => {
   const navigate = useNavigate();
