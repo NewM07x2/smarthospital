80e1df9fd3b2e669813646d201c21b2846b8c30c
Masato Nitta <newmtube07@gmail.com>
2025-10-08 12:32:39 +0900

    delete migrate file

--- laravel-app/database/migrations/2025_10_08_025215_create_failed_jobs_table.php
+++ laravel-app/database/migrations/2025_10_08_025215_create_failed_jobs_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('books', function (Blueprint $table) {
-            //
-            $table->renameColumn('impression', 'review');
+        Schema::create('failed_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
@@ -22,9 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('books', function (Blueprint $table) {
-            //
-            $table->renameColumn('impression', 'review');
-        });
+        Schema::dropIfExists('failed_jobs');
     }
 };
